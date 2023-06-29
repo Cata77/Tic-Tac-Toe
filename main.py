1fef071be5a963ef3b406ca6dc2d89fee717faac
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-06-29 17:47:45 +0300

    Create a bot that user can play against

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import random
+
 matrix = list()
+occupied_cells = list()
 
 
 def print_matrix(matrix):
@@ -12,16 +15,7 @@ def check_occupied_cell(x, y):
     return True if matrix[x][y] != ' ' else False
 
 
-def define_current_symbol(x, y):
-    nr_of_x = sum(cell == 'X' for rows in matrix for cell in rows)
-    nr_of_o = sum(cell == 'O' for rows in matrix for cell in rows)
-    if nr_of_x <= nr_of_o:
-        matrix[x][y] = 'X'
-    else:
-        matrix[x][y] = 'O'
-
-
-def check_state_of_the_game():
+def check_win_condition():
     # check on the rows
     for rows in matrix:
         first_element = rows[0]
@@ -65,35 +59,56 @@ def check_empty_cells_remaining():
     return True if any(cells == ' ' for rows in matrix for cells in rows) else False
 
 
+def check_state_of_game():
+    is_completed, symbol = check_win_condition()
+    if is_completed is not None:
+        print(f'{symbol} wins')
+        exit()
+    elif not check_empty_cells_remaining():
+        print('Draw')
+        exit()
+
+
+def ai_move():
+    print('Making move level "easy"')
+    while True:
+        i = random.randint(0, 2)
+        j = random.randint(0, 2)
+        if (i, j) not in occupied_cells:
+            matrix[i][j] = 'O'
+            occupied_cells.append((i, j))
+            break
+
+
 def main():
-    for i in range(0, 9, 3):
-        matrix.append(list(' '))
+    cells = '_________'.replace('_', ' ')
+    for i in range(0, len(cells), 3):
+        matrix.append(list(cells[i:i + 3]))
 
     print_matrix(matrix)
     while True:
         try:
             x, y = map(int, input('Enter the coordinates: ').split())
+
         except ValueError:
             print('You should enter numbers!')
         else:
             if not (1 <= x <= 3) or not (1 <= y <= 3):
                 print('Coordinates should be from 1 to 3!')
+                continue
             elif check_occupied_cell(x - 1, y - 1):
                 print('This cell is occupied! Choose another one!')
+                continue
             else:
                 x -= 1
                 y -= 1
-                define_current_symbol(x, y)
+                matrix[x][y] = 'X'
                 print_matrix(matrix)
-                is_completed, symbol = check_state_of_the_game()
-                if is_completed is not None:
-                    print(f'{symbol} wins')
-                    break
-                elif check_empty_cells_remaining():
-                    print('Game not finished')
-                else:
-                    print('Draw')
-                    break
+                check_state_of_game()
+                occupied_cells.append((x, y))
+                ai_move()
+                print_matrix(matrix)
+                check_state_of_game()
 
 
 if __name__ == '__main__':
