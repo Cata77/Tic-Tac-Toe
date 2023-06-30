d2bff2fa7130f57ca340fea44c5712f9b60cad4f
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-06-30 20:23:42 +0300

    Refactor code + add medium difficulty ai

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
+from exception import CoordinatesError, OccupiedCellError, EmptyInputError
 
-commands = ['start', 'easy', 'user']
+commands = ['start', 'user']
+difficulty = ['easy', 'medium']
 
 
 def print_matrix(matrix):
@@ -14,7 +16,7 @@ def check_occupied_cell(matrix, x, y):
     return True if matrix[x][y] != ' ' else False
 
 
-def check_win_condition(matrix):
+def check_state_of_game(matrix):
     # Check rows
     for row in matrix:
         if row.count(row[0]) == len(row) and row[0] != ' ':
@@ -30,9 +32,9 @@ def check_win_condition(matrix):
         return 'win', matrix[0][0]
 
     # Check secondary diagonal
-    if all(matrix[i][len(matrix)-1-i] == matrix[0][len(matrix)-1] and
-           matrix[0][len(matrix)-1] != ' ' for i in range(len(matrix))):
-        return 'win', matrix[0][len(matrix)-1]
+    if all(matrix[i][len(matrix) - 1 - i] == matrix[0][len(matrix) - 1]
+           and matrix[0][len(matrix) - 1] != ' ' for i in range(len(matrix))):
+        return 'win', matrix[0][len(matrix) - 1]
 
     return None, None
 
@@ -41,96 +43,122 @@ def check_empty_cells_remaining(matrix):
     return any(cell == ' ' for row in matrix for cell in row)
 
 
-def check_state_of_game(matrix):
-    is_completed, symbol = check_win_condition(matrix)
-    if is_completed is not None:
-        print(f'{symbol} wins')
-        exit()
-    elif not check_empty_cells_remaining(matrix):
-        print('Draw')
-        exit()
+def check_if_logic_move_is_possible(matrix: list[list[str]],
+        free_cells: list[tuple[int, int]], player_symbol: str, opponent_symbol: str) -> bool:
+    copy_free_cells = list(free_cells)
+
+    while len(copy_free_cells):
+        x, y = random.choice(copy_free_cells)
+        matrix[x][y] = player_symbol
+        if check_state_of_game(matrix)[0] == 'win':
+            print_matrix(matrix)
+            free_cells.remove((x, y))
+            return True
+        matrix[x][y] = opponent_symbol
+        if check_state_of_game(matrix)[0] == 'win':
+            matrix[x][y] = player_symbol
+            print_matrix(matrix)
+            free_cells.remove((x, y))
+            print((x, y))
+            return True
+        matrix[x][y] = ' '
+        copy_free_cells.remove((x, y))
+    return False
 
 
-def ai_move(matrix):
-    print('Making move level "easy"')
+def user_move(matrix: list[list[str]], free_cells: list[tuple[int, int]], player_turn: int) -> int:
     while True:
-        i = random.randint(0, 2)
-        j = random.randint(0, 2)
-        if matrix[i][j] == ' ':
-            matrix[i][j] = 'O'
-            break
+        try:
+            coordinates = input('Enter the coordinates: ')
+            if coordinates.strip() == '':
+                raise EmptyInputError
+
+            x, y = map(int, coordinates.split())
+            x -= 1
+            y -= 1
+            if not (0 <= x <= 2) or not (0 <= y <= 2):
+                raise CoordinatesError
+            elif check_occupied_cell(matrix, x, y):
+                raise OccupiedCellError
+            else:
+                matrix[x][y] = 'X' if player_turn % 2 == 1 else 'O'
+                free_cells.remove((x, y))
+                print_matrix(matrix)
+                return player_turn + 1
+        except ValueError:
+            print('You should enter numbers!')
+        except CoordinatesError as err:
+            print(err)
+        except OccupiedCellError as err:
+            print(err)
+        except EmptyInputError as err:
+            print(err)
 
 
-def user_vs_ai(user='first'):
+def ai_move(matrix: list[list[str]], free_cells: list[tuple[int, int]], difficulty: str, player_turn: int) -> int:
+    if difficulty == 'easy':
+        print('Making move level "easy"')
+        x, y = random.choice(free_cells)
+        free_cells.remove((x, y))
+        matrix[x][y] = 'X' if player_turn % 2 == 1 else 'O'
+        print_matrix(matrix)
+        return player_turn + 1
+    elif difficulty == 'medium':
+        print('Making move level "medium"')
+        if player_turn % 2 == 1:
+            player_symbol = 'X'
+            opponent_symbol = 'O'
+        else:
+            player_symbol = 'O'
+            opponent_symbol = 'X'
+
+        if check_if_logic_move_is_possible(matrix, free_cells, player_symbol, opponent_symbol):
+            return player_turn + 1
+        else:
+            x, y = random.choice(free_cells)
+            free_cells.remove((x, y))
+            matrix[x][y] = 'X' if player_turn % 2 == 1 else 'O'
+            print_matrix(matrix)
+            return player_turn + 1
+
+
+def choose_game_mode(player1: str, player2: str):
     matrix = [[' ' for _ in range(3)] for _ in range(3)]
     print_matrix(matrix)
-    if user == 'first':
-        while True:
-            try:
-                x, y = map(int, input('Enter the coordinates: ').split())
-            except ValueError:
-                print('You should enter numbers!')
-            else:
-                if not (1 <= x <= 3) or not (1 <= y <= 3):
-                    print('Coordinates should be from 1 to 3!')
-                    continue
-                elif check_occupied_cell(matrix, x - 1, y - 1):
-                    print('This cell is occupied! Choose another one!')
-                    continue
-                else:
-                    x -= 1
-                    y -= 1
-                    matrix[x][y] = 'X'
-                    print_matrix(matrix)
-                    check_state_of_game(matrix)
-                    ai_move(matrix)
-                    print_matrix(matrix)
-                    check_state_of_game(matrix)
-    else:
-        while True:
-            ai_move(matrix)
-            print_matrix(matrix)
-            check_state_of_game(matrix)
-            try:
-                x, y = map(int, input('Enter the coordinates: ').split())
-            except ValueError:
-                print('You should enter numbers!')
-            else:
-                if not (1 <= x <= 3) or not (1 <= y <= 3):
-                    print('Coordinates should be from 1 to 3!')
-                    continue
-                elif check_occupied_cell(matrix, x - 1, y - 1):
-                    print('This cell is occupied! Choose another one!')
-                    continue
-                else:
-                    x -= 1
-                    y -= 1
-                    matrix[x][y] = 'X'
-                    print_matrix(matrix)
-                    check_state_of_game(matrix)
 
-
-def ai_vs_ai():
-    matrix = [[' ' for _ in range(3)] for _ in range(3)]
+    player_turn = 1
+    free_cells = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
     while True:
-        ai_move(matrix)
-        print_matrix(matrix)
-        check_state_of_game(matrix)
+        if player1 == 'user':
+            player_turn = user_move(matrix, free_cells, player_turn)
+        elif player1 in difficulty:
+            player_turn = ai_move(matrix, free_cells, player1, player_turn)
+        if check_state_of_game(matrix)[0] is not None or not check_empty_cells_remaining(matrix):
+            break
+        if player2 == 'user':
+            player_turn = user_move(matrix, free_cells, player_turn)
+        elif player2 in difficulty:
+            player_turn = ai_move(matrix, free_cells, player2, player_turn)
+        if check_state_of_game(matrix)[0] is not None or not check_empty_cells_remaining(matrix):
+            break
+
+    is_completed, symbol = check_state_of_game(matrix)
+    if is_completed is not None:
+        print(f'{symbol} wins')
+    elif not check_empty_cells_remaining(matrix):
+        print('Draw')
+    main()
 
 
 def main():
     while True:
         command = input('Input command: ').split()
         if command[0] == 'exit':
-            break
-        elif len(command) != 3 or any(elem not in commands for elem in command):
+            exit()
+        elif len(command) != 3 or any(elem not in commands and elem not in difficulty for elem in command):
             print('Bad parameters!')
-        elif command[1] == 'user' and command[2] == 'easy':
-            user_vs_ai()
-        elif command[1] == 'user' and command[2] == 'easy':
-            user_vs_ai('second')
-        elif command[1] == 'user' and command[2] == 'easy':
-            ai_vs_ai()
+        else:
+            choose_game_mode(command[1], command[2])
 
 
 if __name__ == '__main__':
